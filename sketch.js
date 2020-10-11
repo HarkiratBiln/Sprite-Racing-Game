af69d1aca57e01649cde7134ede1cd50e09a1a29
HarkiratBiln <harkiratbiln@gmail.com>
2020-10-11 16:57:53 -0700

    learned about firebase and real time data

--- sketch.js
+++ sketch.js
@@ -1,29 +1,52 @@
 var ball;
+var database, position;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    ball = createSprite(250,250,20,20);
+    ball.shapeColor = "purple";
+
+    database = firebase.database();
+
+    var loc = database.ref("ball/position");
+    loc.on("value",readOP,showErr);
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-5,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(5,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-5);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+5);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x: ball.x + x,
+        y: ball.y + y
+    })
+
+}
+
+function readOP(data) {
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+
+function showErr() {
+    console.log("ERROR");
 }
