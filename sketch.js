d8027fff592fd61e8dcc286605881427e753dece
HarkiratBiln <harkiratbiln@gmail.com>
2020-10-20 16:52:55 -0700

    Sprite Racing

--- sketch.js
+++ sketch.js
@@ -1,52 +1,35 @@
-var ball;
-var database, position;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,20,20);
-    ball.shapeColor = "purple";
-
+    createCanvas(displayWidth-20,displayHeight-130);
     database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
 
-    var loc = database.ref("ball/position");
-    loc.on("value",readOP,showErr);
 
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-5,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(5,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-5);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+5);
-    }
-    drawSprites();
+if(playerCount === 4) {
+    game.update(1);
 }
 
-function writePosition(x,y){
-    //ball.x = ball.x + x;
-    //ball.y = ball.y + y;
-    database.ref("ball/position").set({
-        x: ball.x + x,
-        y: ball.y + y
-    })
+if(gameState === 1) {
+    clear();
+    game.play();
 
 }
-
-function readOP(data) {
-    position = data.val();
-    ball.x = position.x;
-    ball.y = position.y;
-
 }
 
-function showErr() {
-    console.log("ERROR");
-}
